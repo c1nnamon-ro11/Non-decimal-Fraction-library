96a2f047585938cdd614df88fd3fe9ec736b2781
c1nnamon-ro11 <vladislaw.vlad2@gmail.com>
2022-06-25 14:48:39 +0300

    Add project files.

---
+++ Fraction.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Fractions
+{
+    // Class for fraction representation
+    public class Fraction
+    {
+        // Fraction properties
+        public long Numerator { get; set; }      // Fraction numerator (signed)
+        public long Denominator { get; set; }    // Fraction denominator (signed)
+
+        // Fraction constructors
+        /// <summary>
+        /// Fraction constructor by default
+        /// Equals 1 (1/1)
+        /// </summary>
+        public Fraction()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        /// <summary>
+        /// Fraction constructor with numerator and denominator as parameter
+        /// </summary>
+        /// <param name="numerator"></param>
+        /// <param name="denominator"></param>
+        /// <exception cref="DivideByZeroException"></exception>
+        public Fraction(long numerator, long denominator)
+        {
+            InitializeFraction(numerator, denominator);
+        }
+
+        /// <summary>
+        /// Fraction constructor with string as parameter
+        /// </summary>
+        /// <param name="number"></param>
+        /// <exception cref="DivideByZeroException">Throws if denominator equals zero</exception>
+        public Fraction(string fractionString)
+        {
+            // Get numerator and denominator from string 
+            var fractionParts = fractionString.Split('/');
+            // In case if numerator or denominator not integer numbers will be thrown exception
+            int numerator = int.Parse(fractionParts[0]);
+            int denominator = int.Parse(fractionParts[1]);
+            InitializeFraction(numerator, denominator);
+        }
+
+        /// <summary>
+        /// Helping constructor method
+        /// </summary>
+        /// <param name="numerator">Fraction numerator</param>
+        /// <param name="denominator">Fraction denominator</param>
+        /// <exception cref="DivideByZeroException"></exception>
+        private void InitializeFraction(long numerator, long denominator)
+        {
+            // Throw DivideByZeroExeption() if denominator of entered fraction equals zero (x/0)
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            // Change sigh for simplifing calculations
+            if (denominator < 0)
+            {
+                denominator = -denominator;
+                numerator = -numerator;
+            }
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        /// <summary>
+        /// Find the largest common divisor for fraction simplification
+        /// </summary>
+        /// <param name="numerator">Fraction numerator</param>
+        /// <param name="denominator">Fraction denominator</param>
+        /// <returns>Largest common divisor of 2 numbers</returns>
+        private long LargestCommonDivisor(long numerator, long denominator)
+        {
+            while (numerator != denominator)
+            {
+                if (numerator == 0)
+                    break;
+                if (numerator > denominator)
+                    numerator = numerator - denominator;
+                else denominator = denominator - numerator;
+            }
+            return numerator;
+        }
+
+        /// <summary>
+        /// Simplifying fraction in case if it have negative numerator and denominator or numerator greaten that denominator
+        /// </summary>
+        /// <param name="convertToMixedNumber">Flag for converting improper fraction to mixed number</param>
+        public void FractionSimplification(bool convertToMixedNumber)
+        {
+            if (convertToMixedNumber)
+            {
+                long divisor = LargestCommonDivisor(Math.Abs(Numerator), Math.Abs(Denominator));
+                Numerator = Numerator / divisor;
+                Denominator = Denominator / divisor;
+            }
+            // Remove "-" if numerator and denominator negative      
+            if (Numerator < 0 && Denominator < 0)
+            {
+                Numerator = -Numerator; Denominator = -Denominator;
+            }
+            // Case if fraction equal zero
+            if (Numerator == 0)
+            {
+                Denominator = 1;
+            }
+        }
+
+        /// <summary>
+        /// Converting fraction to string representation as mixed number
+        /// </summary>
+        public string PrintMixedNumber()
+        {
+            // Save values for printing
+            long numerator = Numerator;
+            long denominator = Denominator;
+            long integer = 0;
+            string sign;
+
+            // Calculate mixed number from improper fraction 
+            if (numerator < 0)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "+";
+            }
+            numerator = Math.Abs(numerator);
+            while (numerator >= denominator)
+            {
+                integer++;
+                numerator = numerator - denominator;
+            }
+            // Create string as mixed number
+            if (integer == 0 && numerator == 0)
+            {
+                return "0";
+            }
+            if (integer == 0)
+            {
+                return $"{sign}{numerator}/{denominator}";
+            }
+            if (numerator == 0)
+            {
+                return sign + integer;
+            }
+            return $"{sign}{integer} {numerator}/{denominator}";
+        }
+
+        /// <summary>
+        /// Converting fraction to string representation
+        /// </summary>
+        /// <returns>Fraction as string</returns>
+        public string ConvertFractionToString()
+        {
+            if (Numerator == 0)
+            {
+                return "0";
+            }
+            return $"{Numerator}/{Denominator}";
+        }
+    }
+}
---
+++ FractionCalculation.cs
@@ -0,0 +1,115 @@
+﻿namespace Fractions
+{
+    // Class for calculation 
+    // Supports four main operations
+    public static class FractionCalculation
+    {
+        /// <summary>
+        /// Calculates new common denominator of two fractals 
+        /// </summary>
+        /// <param name="denominatorOfFirstFraction">First fraction denominator</param>
+        /// <param name="denominatorOfSecondFraction">Second fraction denominator</param>
+        /// <returns>Common denominator of two fractals</returns>
+        static long GetCommonDedominator(long denominatorOfFirstFraction, long denominatorOfSecondFraction)
+        {
+            long commonDedominator;
+            long newDenominator;
+            long multiplier = 2; // counter for newDenominator in case if denominators are not multiplies
+
+            // Case if denominators of fractions are multiples
+            if (denominatorOfFirstFraction > denominatorOfSecondFraction)
+            {
+                commonDedominator = denominatorOfFirstFraction;
+                if (commonDedominator % denominatorOfSecondFraction == 0)
+                {
+                    return commonDedominator;
+                }
+            }
+            else
+            {
+                commonDedominator = denominatorOfSecondFraction;
+                if (commonDedominator % denominatorOfFirstFraction == 0)
+                {
+                    return commonDedominator;
+                }
+            }
+
+            // Case if denominators of fractions are not multiples
+            while (true)
+            {
+                newDenominator = commonDedominator * multiplier;
+                if (newDenominator % denominatorOfFirstFraction == 0 && newDenominator % denominatorOfSecondFraction == 0)
+                {
+                    return newDenominator;
+                }
+                multiplier++;
+            }
+        }
+
+        /// <summary>
+        /// Adding of two fractions
+        /// </summary>
+        /// <param name="firstFraction">First fraction</param>
+        /// <param name="secondFraction">Second fraction</param>
+        /// <param name="simplifyResult">Flag for simplifiong result of improper fraction</param>
+        /// <returns>Fraction as result of adding</returns>
+        public static Fraction AddFractions(Fraction firstFraction, Fraction secondFraction, bool simplifyResult = false)
+        {
+            Fraction resultOfCalculation = new Fraction();
+            resultOfCalculation.Denominator = GetCommonDedominator(firstFraction.Denominator, secondFraction.Denominator);
+            resultOfCalculation.Numerator =
+                firstFraction.Numerator * (resultOfCalculation.Denominator / firstFraction.Denominator) + secondFraction.Numerator * (resultOfCalculation.Denominator / secondFraction.Denominator);
+            resultOfCalculation.FractionSimplification(simplifyResult);
+            return resultOfCalculation;
+        }
+
+        /// <summary>
+        /// Substraction of two fractions
+        /// </summary>
+        /// <param name="firstFraction">First fraction</param>
+        /// <param name="secondFraction">Second fraction</param>
+        /// <param name="simplifyResult">Flag for simplifiong result of improper fraction</param>
+        /// <returns>Fraction as result of substraction</returns>
+        public static Fraction SubstactFractions(Fraction firstFraction, Fraction secondFraction, bool simplifyResult = false)
+        {
+            Fraction resultOfCalculation = new Fraction();
+            resultOfCalculation.Denominator = GetCommonDedominator(firstFraction.Denominator, secondFraction.Denominator);
+            resultOfCalculation.Numerator =
+                firstFraction.Numerator * (resultOfCalculation.Denominator / firstFraction.Denominator) - secondFraction.Numerator * (resultOfCalculation.Denominator / secondFraction.Denominator);
+            resultOfCalculation.FractionSimplification(simplifyResult);
+            return resultOfCalculation;
+        }
+
+        /// <summary>
+        /// Multiplication of two fractions
+        /// </summary>
+        /// <param name="firstFraction">First fraction</param>
+        /// <param name="secondFraction">Second fraction</param>
+        /// <param name="simplifyResult">Flag for simplifiong result of improper fraction</param>
+        /// <returns>Fraction as result of multiplication</returns>
+        public static Fraction MultiplyFractions(Fraction firstFraction, Fraction secondFraction, bool simplifyResult = false)
+        {
+            Fraction resultOfCalculation = new Fraction();
+            resultOfCalculation.Denominator = firstFraction.Denominator * secondFraction.Denominator;
+            resultOfCalculation.Numerator = firstFraction.Numerator * secondFraction.Numerator;
+            resultOfCalculation.FractionSimplification(simplifyResult);
+            return resultOfCalculation;
+        }
+
+        /// <summary>
+        /// Division of two fractions
+        /// </summary>
+        /// <param name="firstFraction">First fraction</param>
+        /// <param name="secondFraction">Second fraction</param>
+        /// <param name="simplifyResult">Flag for simplifiong result of improper fraction</param>
+        /// <returns>Fraction as result of dividing</returns>
+        public static Fraction DivideFractions(Fraction firstFraction, Fraction secondFraction, bool simplifyResult = false)
+        {
+            Fraction resultOfCalculation = new Fraction();
+            resultOfCalculation.Denominator = firstFraction.Denominator * secondFraction.Numerator;
+            resultOfCalculation.Numerator = firstFraction.Numerator * secondFraction.Denominator;
+            resultOfCalculation.FractionSimplification(simplifyResult);
+            return resultOfCalculation;
+        }
+    }
+}
